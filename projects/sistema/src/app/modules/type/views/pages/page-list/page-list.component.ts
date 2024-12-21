8bb340b3cdfc66fd5c8ac285af531c511d875adf
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-12-21 02:44:15 -0500

    page list for type, just listing and filtering

--- projects/sistema/src/app/modules/type/views/pages/page-list/page-list.component.ts
+++ projects/sistema/src/app/modules/type/views/pages/page-list/page-list.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { MetaColumn } from '../../../../../shared/interfaces/metacolumn.interface';
-import { RequestDto } from '../../../../../shared/application/dtos/request.dto';
+import {
+  PageRequestDto,
+  RequestDto,
+} from '../../../../../shared/application/dtos/request.dto';
 import { Response } from '../../../../../shared/domain/response';
-import { Type } from '../../../domain/type/type';
+import { Type, TypeDisplay } from '../../../domain/type/type';
 import { UtilsService } from '../../../../../shared/services/utils.service';
 import { TypeApplication } from '../../../application/type/type.application';
 import { environment } from '../../../../../../environments/environment';
+import { ExceptionDto } from '../../../../../shared/application/dtos/exception.dto';
 
 @Component({
   selector: 'tw-page-list',
@@ -26,6 +30,8 @@ export class PageListComponent {
   loading: boolean;
   pluralEntity = 'tipos';
 
+  private readonly QUERY_REQUIRED = 'activated<eq>true';
+
   constructor(
     private readonly utilsService: UtilsService,
     private readonly typeApplication: TypeApplication
@@ -40,15 +46,67 @@ export class PageListComponent {
         page: environment.pageIndex,
         size: environment.pageSize,
       },
-      query: 'activated<eq>true',
+      query: this.QUERY_REQUIRED,
     };
   }
 
-  private loadData() {}
+  private loadData() {
+    this.loading = true;
 
-  showModalWindow() {}
+    setTimeout(() => {
+      this.typeApplication.list(this.requestDto).subscribe({
+        next: (response: Response<Type>) => {
+          this.response = response;
+        },
+        error: (error: ExceptionDto) => {
+          this.loading = false;
+        },
+        complete: () => {
+          this.loading = false;
+        },
+      });
+    }, 1000);
+  }
 
-  filter($event) {}
+  paginate($event: PageRequestDto) {
+    if (this.requestDto.page === $event) return;
 
-  paginate($event) {}
+    this.requestDto.page = $event;
+    this.loadData();
+  }
+
+  filter($event: string) {
+    if (this.requestDto.query === $event) return;
+
+    this.requestDto.query = $event
+      ? `${this.QUERY_REQUIRED}:and:${$event}`
+      : this.QUERY_REQUIRED;
+    this.loadData();
+  }
+
+  showModalWindow(row?: any) {
+    console.log('row', row);
+    console.log(this.response);
+  }
+
+  delete(id?: number) {
+    console.log('id to delete', id);
+  }
+
+  get dataSource(): Array<TypeDisplay> {
+    if (!this.response) return [];
+
+    let { content: types, pageIndex, pageSize } = this.response;
+
+    types = !Array.isArray(types) || !types ? [] : types;
+
+    let i = pageIndex * pageSize;
+    const typesDisplay = types.map((type: Type): TypeDisplay => {
+      i++;
+      const typeDisplay: TypeDisplay = type.display();
+      return { ...typeDisplay, '#': i };
+    });
+
+    return typesDisplay;
+  }
 }
