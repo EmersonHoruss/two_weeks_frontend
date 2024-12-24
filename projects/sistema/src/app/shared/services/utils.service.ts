83899d594e6dfe9b70ec72f2c4173052a6f47b21
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-12-24 03:47:29 -0500

    util service now manage informative and loading openings of modals

--- projects/sistema/src/app/shared/services/utils.service.ts
+++ projects/sistema/src/app/shared/services/utils.service.ts
@@ -2,9 +2,38 @@ import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ConfirmComponent } from '../components/confirm/confirm.component';
 import { Observable } from 'rxjs';
+import { LoadingComponent } from '../components/loading/loading.component';
+import {
+  Informative,
+  InformativeComponent,
+} from '../components/informative/informative.component';
+
+export enum OperationType {
+  Creation = 'Creation',
+  Updating = 'Updating',
+  Deletion = 'Deletion',
+}
+
+export enum OperationState {
+  Error = 'Error',
+  Success = 'Success',
+}
 
 @Injectable()
 export class UtilsService {
+  private readonly DEFAULT_CONFIRM_MESSAGE = '¿Estás seguro de continuar?';
+
+  private readonly DEFAULT_SUCCESS_CREATION_MESSAGE = 'Creación exitosa';
+  private readonly DEFAULT_ERROR_CREATION_MESSAGE = 'Creación fallida';
+
+  private readonly DEFAULT_SUCCESS_UPDATING_MESSAGE = 'Actualización exitosa';
+  private readonly DEFAULT_ERROR_UPDATING_MESSAGE = 'Actualización fallida';
+
+  private readonly DEFAULT_SUCCESS_DELETION_MESSAGE = 'Eliminación exitosa';
+  private readonly DEFAULT_ERROR_DELETION_MESSAGE = 'Eliminación fallida';
+
+  private readonly DEFAULT_SIZE = '340px'; // it looks good in mobile devices
+
   constructor(private readonly dialog: MatDialog) {}
 
   showModalWindow(
@@ -14,9 +43,9 @@ export class UtilsService {
     return this.dialog.open(classComponent, options);
   }
 
-  showConfirm(message: string = ''): Observable<any> {
+  showConfirm(message: string = this.DEFAULT_CONFIRM_MESSAGE): Observable<any> {
     const reference = this.dialog.open(ConfirmComponent, {
-      width: '340px', // it looks good in mobile devices
+      width: this.DEFAULT_SIZE,
       disableClose: true,
     });
 
@@ -25,5 +54,57 @@ export class UtilsService {
     }
 
     return reference.afterClosed();
+  }
+
+  showInformative(
+    type: OperationType,
+    state: OperationState,
+    message: string = ''
+  ): void {
+    let finalMessage = this.getInformativeMessage(type, state, message);
+    const data: Informative = {
+      state,
+      message: finalMessage,
+    };
+    this.dialog.open(InformativeComponent, {
+      width: this.DEFAULT_SIZE,
+      disableClose: true,
+      data,
+    });
+  }
+
+  private getInformativeMessage(
+    type: OperationType,
+    state: OperationState,
+    message: string
+  ): string {
+    if (message === '') {
+      if (type === OperationType.Creation && state === OperationState.Success)
+        return this.DEFAULT_SUCCESS_CREATION_MESSAGE;
+
+      if (type === OperationType.Creation && state === OperationState.Error)
+        return this.DEFAULT_ERROR_CREATION_MESSAGE;
+
+      if (type === OperationType.Updating && state === OperationState.Success)
+        return this.DEFAULT_SUCCESS_UPDATING_MESSAGE;
+
+      if (type === OperationType.Updating && state === OperationState.Error)
+        return this.DEFAULT_ERROR_UPDATING_MESSAGE;
+
+      if (type === OperationType.Deletion && state === OperationState.Success)
+        return this.DEFAULT_SUCCESS_DELETION_MESSAGE;
+
+      if (type === OperationType.Deletion && state === OperationState.Error)
+        return this.DEFAULT_ERROR_DELETION_MESSAGE;
+    }
+
+    return message;
+  }
+
+  showLoading(): MatDialogRef<LoadingComponent> {
+    return this.dialog.open(LoadingComponent, {
+      width: this.DEFAULT_SIZE,
+      disableClose: true,
+    });
   }
 }
