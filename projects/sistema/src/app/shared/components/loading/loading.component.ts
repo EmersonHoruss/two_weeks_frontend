8f5a2fb3918b3ffe7e6ba9c233ce9d653098e48f
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2024-12-24 03:46:12 -0500

    loading dialog component

---
+++ projects/sistema/src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tw-loading',
+  standalone: false,
+
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss',
+})
+export class LoadingComponent {}
