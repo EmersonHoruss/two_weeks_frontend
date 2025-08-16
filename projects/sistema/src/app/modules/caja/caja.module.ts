efbb073b66b7d9d3c418d46cd5f96d3f431235b3
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-08-16 01:22:37 -0500

    form of open caja in progress

--- projects/sistema/src/app/modules/caja/caja.module.ts
+++ projects/sistema/src/app/modules/caja/caja.module.ts
@@ -6,8 +6,9 @@ import { PageListComponent } from './views/pages/page-list/page-list.component';
 import { SharedModule } from '../../shared/shared.module';
 import { PageOpenComponent } from './views/pages/page-open/page-open.component';
 import { PageUpdateComponent } from './views/pages/page-update/page-update.component';
+import { FormComponent } from './views/components/form/form.component';
 
-const components = [];
+const components = [FormComponent];
 const pages = [PageOpenComponent, PageUpdateComponent, PageListComponent];
 const declarations = [...components, ...pages];
 const imports = [CommonModule, CajaRoutingModule, SharedModule];
---
+++ projects/sistema/src/app/modules/caja/views/components/form/form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tw-form',
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+  standalone: false,
+})
+export class FormComponent {
+  constructor() {}
+}
