3ef24449a5b5ebf3738521d767040dbce44e4026
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-08-16 00:17:06 -0500

    caja historical

--- projects/sistema/src/app/modules/caja/caja.module.ts
+++ projects/sistema/src/app/modules/caja/caja.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CajaRoutingModule } from './caja-routing.module';
+import { PageRegistrarComponent } from './views/pages/page-registrar/page-registrar.component';
+import { PageListComponent } from './views/pages/page-list/page-list.component';
+import { SharedModule } from '../../shared/shared.module';
+
+const components = [];
+const pages = [PageRegistrarComponent, PageListComponent];
+const declarations = [...components, ...pages];
+const imports = [CommonModule, CajaRoutingModule, SharedModule];
+const providers = [];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CajaRoutingModule],
+  declarations,
+  imports,
+  providers,
 })
 export class CajaModule {}
--- projects/sistema/src/app/modules/caja/views/pages/page-list/page-list.component.ts
+++ projects/sistema/src/app/modules/caja/views/pages/page-list/page-list.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { MetaColumn } from '../../../../../shared/interfaces/metacolumn.interface';
+import {
+  PageRequestDto,
+  RequestDto,
+} from '../../../../../shared/application/dtos/request.dto';
+import { Response } from '../../../../../shared/domain/response';
+import { Caja, CajaDisplay } from '../../../domain/caja/caja';
+import { environment } from '../../../../../../environments/environment';
+import { CajaApplication } from '../../../application/caja/caja.application';
+import { ExceptionDto } from '../../../../../shared/application/dtos/exception.dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'tw-page-list',
@@ -6,4 +17,85 @@ import { Component } from '@angular/core';
   styleUrl: './page-list.component.scss',
   standalone: false,
 })
-export class PageListComponent {}
+export class PageListComponent {
+  metaColumns: Array<MetaColumn> = [
+    { field: '#', title: '#', isVisible: true },
+    { field: 'fecha', title: 'Fecha', isVisible: true },
+    { field: 'montoInicial', title: 'Monto Inicial', isVisible: true },
+    { field: 'montoFinalFisico', title: 'Monto Final Físico', isVisible: true },
+    {
+      field: 'montoFinalDigital',
+      title: 'Monto Final Digital',
+      isVisible: true,
+    },
+    { field: 'ganacia', title: 'Ganancia', isVisible: true },
+  ];
+
+  requestDto: RequestDto;
+  response: Response<Caja>;
+
+  loading: boolean = true;
+  pluralEntity: string = 'cajas';
+
+  constructor(
+    private readonly cajaApplication: CajaApplication,
+    private readonly router: Router
+  ) {
+    this.initializeRequestDto();
+    this.loadData();
+  }
+
+  private initializeRequestDto() {
+    this.requestDto = {
+      page: {
+        page: environment.pageIndex,
+        size: environment.pageSize,
+      },
+      query: '',
+    };
+  }
+
+  private loadData() {
+    this.loading = true;
+
+    this.cajaApplication.list(this.requestDto).subscribe({
+      next: (response: Response<Caja>) => {
+        this.response = response;
+      },
+      error: (error: ExceptionDto) => {
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+      },
+    });
+  }
+
+  paginate($event: PageRequestDto) {
+    if (this.requestDto.page === $event) return;
+
+    this.requestDto.page = $event;
+    this.loadData();
+  }
+
+  createOrUpdate() {
+    this.router.navigate(['/caja/abrir']);
+  }
+
+  get dataSource(): Array<CajaDisplay> {
+    if (!this.response) return [];
+
+    let { content: cajas, pageIndex, pageSize } = this.response;
+
+    cajas = !Array.isArray(cajas) || !cajas ? [] : cajas;
+
+    let i = pageIndex * pageSize;
+    const cajasDisplay = cajas.map((caja: Caja): CajaDisplay => {
+      i++;
+      const cajaDisplay: CajaDisplay = caja.display();
+      return { ...cajaDisplay, '#': i };
+    });
+
+    return cajasDisplay;
+  }
+}
