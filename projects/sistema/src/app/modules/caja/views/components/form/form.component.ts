8f091ca58ca3609f267365aa6dd96f714388605b
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-08-16 02:36:56 -0500

    form of caja done for creation and updating

--- projects/sistema/src/app/modules/caja/views/components/form/form.component.ts
+++ projects/sistema/src/app/modules/caja/views/components/form/form.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  CajaCreateDto,
+  CajaUpdateDto,
+} from '../../../application/caja/caja.dto';
 
 @Component({
   selector: 'tw-form',
@@ -7,5 +17,42 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class FormComponent {
-  constructor() {}
+  form!: FormGroup;
+  @Input() data: Partial<CajaUpdateDto> | null;
+  @Output() getForm = new EventEmitter<any>();
+
+  constructor(private fb: FormBuilder) {
+    this.initForm();
+  }
+
+  private initForm() {
+    const now = new Date();
+    const offset = -now.getTimezoneOffset();
+    const sign = offset >= 0 ? '+' : '-';
+    const pad = (n: number) => String(Math.floor(Math.abs(n))).padStart(2, '0');
+    const formattedDate = `${now.getFullYear()}-${pad(
+      now.getMonth() + 1
+    )}-${pad(now.getDate())}T${pad(now.getHours())}:${pad(
+      now.getMinutes()
+    )}:${pad(now.getSeconds())}${sign}${pad(offset / 60)}:${pad(offset % 60)}`;
+
+    this.form = this.fb.group({
+      id: new FormControl(this.data?.id),
+      fecha: [
+        { value: this.data?.fecha ?? formattedDate, disabled: true },
+        Validators.required,
+      ],
+      montoInicial: [
+        this.data?.montoInicial ?? 0,
+        [Validators.required, Validators.min(0)],
+      ],
+    });
+  }
+
+  save(): void {
+    if (this.form.valid) {
+      const dto: CajaCreateDto = this.form.getRawValue();
+      this.getForm.emit(dto);
+    }
+  }
 }
